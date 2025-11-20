821314cdea819d09ecc6b038c3ed2fbe443eddec
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-20 11:35:37 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,19 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        for(int i=2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int p){
+        for(int i=2;i<p;i++){
+            if(p%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
