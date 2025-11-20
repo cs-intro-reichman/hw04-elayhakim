27d34834fe589d3ce5a1ad0266df50574b0dd37d
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-20 12:14:35 +0200

    Mystrinf

--- MyString.java
+++ MyString.java
@@ -18,13 +18,35 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newstr = "";
+        for(int i= 0; i<str.length();i++){
+            if(str.charAt(i)>=65 && str.charAt(i)<=90){
+                newstr+= (char)(str.charAt(i)+32);
+            }else{
+                newstr+= str.charAt(i);
+            }
+        } 
+        return newstr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if(str1.length() >= str2.length()){
+            for(int i=0 ; i <= str1.length()-str2.length() ; i++){
+                if(str1.charAt(i) == str2.charAt(0)){
+                    boolean flag = true;
+                    for(int j=0; j<str2.length();j++){
+                        if(str1.charAt(i+j) != str2.charAt(j)){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
