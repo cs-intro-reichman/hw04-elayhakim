6b54849cf2659f464969a8edb24ae9a69efad542
elayhakim <elay.hakim@post.runi.ac.il>
2025-11-20 14:13:05 +0200

    Debuging

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -43,8 +43,10 @@ public class ArrCharOps {
      *  returns true; Otherwise returns false.
      */
     public static boolean equals(char[] arr1, char[] arr2) {
-        int max= Math.max(arr1.length,arr2.length);
-        for(int i=0; i< max; i++){
+        if(arr1.length != arr2.length){
+            return false;
+        }
+        for(int i=0; i< arr1.length; i++){
             if(arr1[i]!= arr2[i]){
                 return false;
             }
